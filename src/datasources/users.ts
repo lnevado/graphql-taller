e9c1b41ac35910b3e84ad462d6a12c642d5e28f4
lucas-nevado-bayer <lucas.nevado.ext@bayer.com>
2026-04-16 12:01:47 -0300

    test memory

--- src/datasources/users.ts
+++ src/datasources/users.ts
@@ -1,7 +1,15 @@
 import { User } from "../types/users";
 
+const sampleUsers: User[] = [
+    { id: "1", name: "John Doe", email: "john.doe@example.com", age: 30 },
+    { id: "2", name: "Jane Doe", email: "jane.doe@example.com", age: 25 },
+    { id: "3", name: "Jane Doe", email: "jane.doe@example.com", age: 25 },
+]
+
+
 export class UsersAPI {
-    getUser(id: string): User {
-        return {id, name: "John Doe", email: "john.doe@example.com", age: 30};
+    getUser(id: string): User | undefined {
+        const user = sampleUsers.find(user => user.id = id);
+        return user;
     }
 }
